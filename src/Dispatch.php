e04bedfd1a1e4a1cb42eb53ed6e2c3734a00909e
Samantha Quiñones <samantha.quinones@teamaol.com>
2014-11-05 17:43:05 -0500

    Added Request & Response getters and routeExists method to allow callers to check for a route's existence prior to dispatching - Refactored the run method some more and added docblocks - NB: I don't know how math works

--- src/Dispatch.php
+++ src/Dispatch.php
@@ -32,6 +32,22 @@ class Dispatch
 	/** @var bool */
 	private $debug_enabled = false;
 
+	/** @var \Aura\Web\Request */
+	private $request;
+
+	/** @var \Aura\Web\Response */
+	private $response;
+
+	/** @var \Aura\Router\Route */
+	private $matched_route;
+
+	/**
+	 * @param Router $router
+	 * @param WebFactory $web_factory
+	 * @param ActionFactoryInterface $action_factory
+	 * @param PresenterInterface $presenter
+	 * @param LoggerInterface $logger
+	 */
 	public function __construct(
 		Router $router,
 		WebFactory $web_factory,
@@ -46,69 +62,173 @@ class Dispatch
 		$this->logger         = $logger;
 	}
 
+	/**
+	 * Returns an instance of Request, creating one if it does not exist
+	 *
+	 * @return Request
+	 */
+	public function getRequest()
+	{
+		if (!isset($this->request)) {
+			$this->request = $this->web_factory->newRequest();
+		}
+		return $this->request;
+	}
+
+	/**
+	 * Returns an instance of Response, creating one if it does not exist
+	 *
+	 * @return Response
+	 */
+	public function getResponse()
+	{
+		if (!isset($this->response)) {
+			$this->response = $this->web_factory->newResponse();
+		}
+		return $this->response;
+	}
+
+	/**
+	 * Searches for the target route on the router, returning it or false if it does not exist.
+	 *
+	 * @return \Aura\Router\Route|false
+	 * @throws \Aura\Web\Exception\InvalidComponent
+	 */
+	protected function matchRoute()
+	{
+		$request = $this->getRequest();
+		$this->matched_route = $this->router->match($request->url->get(PHP_URL_PATH), $request->server->get());
+		return $this->matched_route;
+	}
+
+	/**
+	 * Return true or false if the target route is defined in the router
+	 *
+	 * @return bool
+	 * @throws \Aura\Web\Exception\InvalidComponent
+	 */
+	public function routeExists()
+	{
+		if (!isset($this->matched_route)) {
+			$this->matched_route = $this->matchRoute();
+		}
+		return (bool) $this->matched_route;
+	}
+
+	/**
+	 * Dispatch a request to an action and populate Response
+	 *
+	 * @param ActionInterface $action
+	 * @param Response $response
+	 */
+	protected function dispatch(ActionInterface $action, Response $response)
+	{
+		// Run the response through the action.
+		$action->before();
+		$action($response);
+		$action->after();
+	}
+
+	/**
+	 * Dispatch a response to the presenter
+	 *
+	 * @param ActionInterface $action
+	 * @param Response $response
+	 * @throws Exception
+	 */
+	protected function present(ActionInterface $action, Response $response)
+	{
+		// Make sure this is not a redirect and then run the response through the presenter.
+		if ($response->status->getCode() >= 300 && $response->status->getCode() < 400) {
+			return;
+		}
+
+		$format  = $this->getMedia($action)->getValue();
+		$content = $this->presenter->run($action->getData($format), $format, $action->getView());
+		$response->content->set($content);
+		$response->content->setType($format);
+	}
+
+	/**
+	 * Get the media type for the request
+	 *
+	 * @param ActionInterface $action
+	 * @return \Aura\Accept\Media\MediaValue|false
+	 * @throws Exception
+	 */
+	protected function getMedia(ActionInterface $action)
+	{
+		$available = array_intersect($action->getAllowedFormats(), $this->presenter->getAvailableFormats());
+
+		//@todo don't mix Di and randomly calling factories
+		$accept_factory = new AcceptFactory($_SERVER);
+		$accept         = $accept_factory->newInstance();
+
+		$media = $accept->negotiateMedia($available);
+		if (empty($media)) {
+			throw new Exception('Could not find a compatible content type for response');
+		}
+
+		return $media;
+	}
+
+	/**
+	 * Attempt to match a route and instantiate an Action, bailing out with an exception on failure
+	 *
+	 * @param Request $request
+	 * @return ActionInterface|null
+	 * @throws ActionNotFoundException
+	 * @throws PageNotFoundException
+	 */
+	protected function getAction(Request $request)
+	{
+		// Get the matched route.
+		$route = $this->matched_route ?: $this->matchRoute();
+		if (!$route) {
+			$this->errorRouteNotMatched($request);
+		}
+
+		$params = $route->params;
+
+		// Get the appropriate action.
+		$action = $this->action_factory->newInstance($params['action'], $request, $params);
+		if (!$action) {
+			$this->errorActionNotFound($params['action']);
+		}
+
+		return $action;
+	}
+
+	/**
+	 * Dispatch to an action and handle errors
+	 *
+	 * @throws ExitDispatchException
+	 */
 	public function run()
 	{
-		$params   = [];
-		$view     = null;
-		$request  = $this->web_factory->newRequest();
-		$response = $this->web_factory->newResponse();
+		$request  = $this->getRequest();
+		$response = $this->getResponse();
 
-		try {
-			// Get the matched route.
-			$route = $this->router->match($request->url->get(PHP_URL_PATH), $request->server->get());
-			if (!$route) {
-				$this->errorRouteNotMatched($request);
-			}
-
-			$params = $route->params;
-
-			// Get the appropriate action.
-			$action = $this->action_factory->newInstance($params['action'], $request, $params);
-			if (!$action) {
-				$this->errorActionNotFound($params['action']);
-			}
-
-			// Run the response through the action.
-			$action->before();
-			$action($response);
-			$action->after();
+		// --------------- Dispatch
+		try{
+			$action = $this->getAction($request);
+			$this->dispatch($action, $response);
 		} catch (\Exception $e) {
 			$this->debug('Caught 500: ' . get_class($e) . ': ' . $e->getMessage() . PHP_EOL . $e->getTraceAsString());
 
 			// This is your escape hatch.
 			if ($e instanceof ExitDispatchException) {
 				throw $e;
-			} // Exceptions can implement the action interface to handle themselves.
-			elseif ($e instanceof ActionInterface) {
-				$action = $e;
-			} // When all else fails use the default ATC exception.
-			else {
-				$action = new Exception();
 			}
 
-			$action($response);
+			// Exceptions can implement the action interface to handle themselves. For anything else, use the default
+			$action = ($e instanceof ActionInterface) ? $e : new Exception();
+			$this->dispatch($action, $response);
 		}
 
+		// --------------- Present
 		try {
-			// Make sure this is not a redirect and then run the response through the presenter.
-			if ($response->status->getCode() < 300 || $response->status->getCode() > 399) {
-				$available = array_intersect($action->getAllowedFormats(), $this->presenter->getAvailableFormats());
-
-				//@todo don't mix Di and randomly calling factories
-				$accept_factory = new AcceptFactory($_SERVER);
-				$accept         = $accept_factory->newInstance();
-
-				$media = $accept->negotiateMedia($available);
-				if (empty($media)) {
-					throw new Exception('Could not find a compatible content type for response');
-				} else {
-					$format  = $media->getValue();
-					$content = $this->presenter->run($action->getData($format), $format, $action->getView());
-
-					$response->content->set($content);
-					$response->content->setType($format);
-				}
-			}
+			$this->present($action, $response);
 		} catch (\Exception $e) {
 			$this->debug('Presentation Exception - ' . get_class($e) . ': ' . $e->getMessage());
 			$response->status->set(500);
@@ -116,6 +236,7 @@ class Dispatch
 			$response->content->set($this->errorHtmlResponse());
 		}
 
+		// --------------- Return
 		$this->send($response);
 	}
 
